dfbfad6ab93e00fa075a98691faf6b103801415b
Jisang Yun <journeyofbabo@gmail.com>
2024-09-05 12:51:29 +0900

    solve quize2

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -28,6 +28,30 @@ mod my_module {
 
     // TODO: Complete the function as described above.
     // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output: Vec<String> = Vec::new();
+
+        for (text, command) in input {
+            match command {
+                Command::Uppercase => output.push(text.to_uppercase()),
+                Command::Trim => output.push(text.trim().to_string()),
+                Command::Append(n) => output.push(text + &"bar".repeat(n)),
+            }
+        }
+
+        output
+
+        /*
+        input
+            .into_iter()
+            .map(|(string, command)| match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append(n) => string + &"bar".repeat(n),
+            })
+            .collect()
+         */
+    }
 }
 
 fn main() {
@@ -38,6 +62,7 @@ fn main() {
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
+    use super::my_module::*;
     use super::Command;
 
     #[test]
